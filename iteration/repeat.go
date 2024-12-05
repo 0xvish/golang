b516ca6b558948e921f17765d4fb696f20c892e8
Vishvam Moliya <vishvammoliya@gmail.com>
2024-12-06 01:22:45 +0530

    code(Iteration): v1.1

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,9 @@
 package iteration
 
 // Repeat takes a character and repeats it 5 times
-func Repeat(character string) string {
+func Repeat(character string, n int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < n; i++ {
 		repeated += character
 	}
 	return repeated
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,19 +1,40 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("v")
-	expected := "vvvvv"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
+	t.Run("repeat character different number of times", func(t *testing.T) {
+		repeated := Repeat("v", 1)
+		expected := "v"
 
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+	t.Run("repeat character specific number of times", func(t *testing.T) {
+		repeated := Repeat("v", 7)
+		expected := "vvvvvvv"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("v", 7)
+	fmt.Println(repeated)
+	// Output: vvvvvvv
 }
 
 func BenchmarkRepeat (b *testing.B) {
 	for i := 0; i < b.N ; i++ {
-		Repeat("a")
+		Repeat("a", 10)
 	}
 }
